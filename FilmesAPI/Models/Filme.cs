b1b276bade4e8d4721c402a3f5ac35fe6d878b0b
wcocuzza <wallace.cocuzza@gmail.com>
2023-01-12 12:08:58 -0300

    Add project files.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    // DataAnnottions
+    [Required]
+    public string Titulo { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600)]
+    public int Duracao { get; set; }
+}
